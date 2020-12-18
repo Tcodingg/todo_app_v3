bb8e02c25c3bffd6f56372d6650e12114303ae02
DePrincio <deprincio@DePrincio.local>
2020-12-18 18:26:21 -0500

    add when cliked

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 export default function Todo() {
+  const [item, setItem] = useState('');
+  const [itemList, setItemList] = useState([]);
+  const [isDone, setIsDone] = useState(false);
+
+  function handleChange(event) {
+    event.preventDefault();
+
+    const inputValues = event.target.value;
+    setItem(inputValues);
+  }
+
+  function addBtn(event) {
+    event.preventDefault();
+    setItemList((preValue) => [...preValue, item]);
+    setItem('');
+  }
+  function crossOut() {
+    setIsDone((preValue) => !preValue);
+    console.log(isDone);
+    itemList.filter((x)=>())
+  }
+
   return (
     <div className='container'>
       <div className='wrapper'>
@@ -10,24 +32,24 @@ export default function Todo() {
             <h1>todo app</h1>
           </div>
           <div className='input-wrapper'>
-            <input type='text' />
-            <button>add</button>
+            <input value={item} onChange={handleChange} type='text' />
+            <button onClick={addBtn}>add</button>
           </div>
         </div>
-
         <div className='todo-list'>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
-          <li>first todo</li>
+          {itemList
+            .map((todos, index) => {
+              return (
+                <li
+                  onClick={crossOut}
+                  key={index}
+                  style={{ textDecoration: isDone ? 'line-through' : 'none' }}
+                >
+                  {todos}
+                </li>
+              );
+            })
+            .reverse()}
         </div>
       </div>
     </div>
